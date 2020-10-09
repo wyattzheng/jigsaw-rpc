624c188c58b79fb91bb37ae074bb616dd0348ee6
zhymc <854185073@qq.com>
2020-10-09 23:16:33 +0800

    build: this is a base build of the project

---
+++ src/network/socket/direction-socket.ts
@@ -0,0 +1,10 @@
+import Socket = require("@/network/socket/socket");
+
+
+class DirectionSocket extends Socket{
+	constructor(){
+		
+	}
+}
+
+export = DirectionSocket
---
+++ src/network/socket/socket.ts
@@ -0,0 +1,9 @@
+
+export = abstract class Socket{
+	constructor(){
+		
+	}
+
+};
+
+
